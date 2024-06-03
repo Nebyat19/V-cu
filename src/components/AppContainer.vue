b770a98c9094faf59def6dee04f7cb71e84cf00f
Nebyat Bekele <nebyat.bekele@aastustudent.edu.et>
2024-06-03 22:05:06 +0300

    scrollbar

--- src/components/AppContainer.vue
+++ src/components/AppContainer.vue
@@ -1,6 +1,6 @@
 <template>
   
-    <div class="h-fit pb-5 font-main py-3 px-6 lg:rounded-xl bg-white max-w-sm mx-auto overflow-y-auto overflow-x-hidden">
+    <div class="h-fit pb-5 app-container font-main py-3 px-6 lg:rounded-xl bg-white max-w-sm mx-auto overflow-y-auto overflow-x-hidden">
       <slot></slot>
     </div>
 </template>
