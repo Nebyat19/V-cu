0b7dc61cc106e33e46989f6c700dfde004353f39
Nebyat Bekele <nebyat.bekele@aastustudent.edu.et>
2024-06-05 00:14:33 +0300

    homeView

--- src/components/ui/RoundedCardContainer.vue
+++ src/components/ui/RoundedCardContainer.vue
@@ -1,16 +1,16 @@
 <template>
-  <div :class="['bg-[#d4f6e9]']" class="px-4 rounded-[15px] py-4">
+  <div :class="['bg-[#d4f6e9]']" class="px-2 rounded-[15px] py-2">
     <slot>
       <div class="flex">
-        <span class="flex gap-1 flex-col items-center justify-center pr-4   border-r border-gray-400">
-          <h1 class="text-3xl text-slate-700">03</h1>
+        <span class="flex  flex-col items-center justify-center pr-3   border-r border-gray-400">
+          <h1 class="text-2xl text-slate-700">03</h1>
           <Date>Aug</Date></span
         >
 
-      <div class="flex flex-col w-full gap-3 ml-4">
+      <div class="flex flex-col w-full gap-1 ml-4">
         <div class="flex items-center justify-between ">
           <h2 class="text-lg font-semibold text-slate-800">Hard Day Af</h2>
-          <h1 class="text-xl">😞</h1>
+          <h1 class="text-lg">😞</h1>
         </div>
         <h3 class="text-xs text-textLight">I had a wounderfull day! today was...</h3>
       </div>
@@ -18,25 +18,7 @@
       
     </slot>
   </div>
-  <div :class="[bgColor]" class="px-4 rounded-[15px] py-4">
-    <slot>
-      <div class="flex">
-        <span class="flex gap-1  flex-col items-center justify-center pr-4   border-r border-gray-400">
-          <h1 class="text-3xl text-slate-700">03</h1>
-          <Date>Aug</Date></span
-        >
 
-      <div class="flex flex-col w-full gap-3 ml-4">
-        <div class="flex items-center justify-between ">
-          <h2 class="text-lg font-semibold text-slate-800">Hard Day Af</h2>
-          <h1 class="text-xl">😞</h1>
-        </div>
-        <h3 class="text-xs text-textLight">I had a wounderfull day! today was...</h3>
-      </div>
-      </div>
-      
-    </slot>
-  </div>
 </template>
 
 <script setup>
